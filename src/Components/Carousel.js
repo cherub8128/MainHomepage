c37b34231f1fcc6e7eb3ab868f99783d89aa5e20
cherub8128 <cherub8128@daum.net>
2021-10-18 12:27:36 +0900

    캐러셀 수정

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,5 +1,25 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const tonext = keyframes`
+    75% { left: 0; }
+    95% { left: 100%; }
+    98% { left: 100%; }
+    99% { left: 0%; }
+`;
+const tostart = keyframes`
+    75% { left: 0; }
+    95% { left: -300%; }
+    98% { left: -300%; }
+    99% { left: 0%; }
+`;
+
+const snap = keyframes`
+    96% { scroll-snap-align: center; }
+    97% { scroll-snap-align: none; }
+    99% { scroll-snap-align: none; }
+    100% { scroll-snap-align: center; }
+`;
 
 const CarouselBase = styled.div`
     display: flex;
@@ -13,23 +33,28 @@ const CarouselBase = styled.div`
     }
     height:100%;
     background-color: #fff;
-    
     div {
         scroll-snap-align: start;
         flex: none;
         display: flex;
         width: 100%;
         height: 100%;
-        font-size: 3em;
+        font-size: 1em;
         justify-content: center;
         align-items: center;
         flex-flow: column;
+        animation-name: ${tonext}, ${snap};
+        animation-timing-function: ease;
+        animation-duration: 10s;
+        animation-iteration-count: infinite;
     }
+    div:last-child { animation-name: ${tostart}, ${snap}; }
 `;
 const Container = styled.div`
     position: relative;
     width: 100%;
     height: 70vh;
+    box-shadow: 5px 0px 10px #0D0D0D10;
 `;
 const CarouselButtonSet = styled.div`
     position:absolute;
